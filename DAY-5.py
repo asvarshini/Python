1215420796fb9f2679557ae6e7bb8dc20b488b64
VarshiniAS <varshinias32@gmail.com>
2026-02-14 15:05:23 +0530

    thise my 2nd modification in git

--- DAY-5.py
+++ DAY-5.py
@@ -1,4 +1,4 @@
-'''#polymorphism(many forms same method for diffent behaviour
+#polymorphism(many forms same method for diffent behaviour
 #no overloding (same method name different parameter) overriding (same method name in different class)
 
 class animal:
@@ -68,6 +68,41 @@ class chi(per):
 
 s2=chi()
 #private (using  2 undersccore,cannot acess in class directely)
+# 1(a) acessinfg private members in same class 
+
+class nam:
+    def __init__(self):
+        self.name="var"
+        self.__age =22
+s1=nam()
+print(s1.name)
+print(s1._nam__age)#thise also wrok but not good practice
+#or
+class nam:
+    def __init__(self):
+        self.name="var"
+        self.__age =22
+    def show(self):
+        return self.__age
+s1=nam()
+print(s1.name,s1.show())
+#1 (b) acessing from different class
+class nam:
+    def __init__(self):
+        self.name="ammu"
+        self.__age =26
+    def show(self):
+        return self.__age
+class b(nam):
+    def read(self):
+        print(self.name,self.show())
+s1=b()
+s1.read()
+# 1(c) acess private in child class
+
+
+
+
 class per:
     def __init__(self,__age):
         self.__age=__age
@@ -99,7 +134,7 @@ class stu:
         print("name =",self.name,"age=",self.__age)#orself.set()
 
 s1 =stu("varshini",21)
-s1.get()'''
+s1.get()
 #example for 3 together(private member acn acess freeliy inside same same class,not in child class and another class)
 class  company:
     def __init__(self,name,_accno,__balance):
@@ -122,6 +157,7 @@ s1.deposite(2000)
 s1.withdraw(1000)
 print(s1.name)
 print(s1.set())#private members cannot acess overcome by name mangling
+print("i am learing git")
         
         
 
