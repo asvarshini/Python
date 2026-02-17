66a09ccc288602a7f42730f1c8d876dca27ce44b
VARSHINI <varshinias32@gmail.com>
2026-02-17 17:15:58 +0530

    new changes

---
+++ DAY_8.py
@@ -0,0 +1,36 @@
+'''10 #Design a student management system:
+
+#create a class Student with attributes as like name, roll number and marks. Write the methods to:
+
+# display the student details.
+
+#calculate the grade based on marks
+
+#maintain a list if students and sort them by marks or name.'''
+class studen_mangement:
+    def __init__(self,name,rool,marks):
+        self.name =name
+        self.rool =rool
+        self.marks =marks
+    def display(self):
+        if(self.marks>=90):
+            self.grade ="A"
+        elif(self.marks>= 80 and self.marks <90):
+            self.grade ="B"
+        elif(self.marks>= 70 and self.marks <80):
+            self.grade ="C"
+        else:
+            self.grade="D"
+        print("NAME =",self.name,"ROLL =",self.rool,"MARKS =",self.marks,"GRADE =",self.grade)
+    
+        
+students = [
+    studen_mangement("varshini", 1, 99),
+    studen_mangement("amrutha", 2, 98),
+    studen_mangement("hamsha", 3, 70)
+]
+
+
+for s in sorted(students,key =lambda x:x.name):
+    s.display()
+
